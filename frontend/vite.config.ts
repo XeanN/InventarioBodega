0cf00b1aeb7c11abdcc3a236fa60e6eac701c2b4
n1ckoxd <nicolasqw60@gmail.com>
2026-06-19 15:52:52 -0500

    front2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:3001', // <--- ¡AQUÍ ESTÁ EL CAMBIO IMPORTANTE!
         changeOrigin: true,
       },
     },
